c4d67550b3922f87760804f3ded997805b8d5910
asw3bd3v <asw3bd3v@gmail.com>
2020-10-06 16:03:48 +0300

    Fixed

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,12 +6,14 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Laravel React application</title>
-  <link href="{{mix('react/css/app.css')}}" rel="stylesheet" type="text/css">
+<!--  <link href="{{mix('react/css/app.css')}}" rel="stylesheet" type="text/css">-->
+<link href="/react/css/app.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
   <div id="root"></div>
-  <script src="{{mix('react/js/app.js')}}"></script>
+<!--  <script src="{{mix('react/js/app.js')}}"></script>-->
+  <script src="/react/js/app.js"></script>
 </body>
 
 </html>
