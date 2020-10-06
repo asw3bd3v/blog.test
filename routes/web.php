81fd3816228f65f8b546e36eeb2e5572399eb685
asw3bd3v <asw3bd3v@gmail.com>
2020-10-06 16:10:49 +0300

    Fixed

--- routes/web.php
+++ routes/web.php
@@ -14,22 +14,33 @@
 Route::get('/', function () {
     return view('app');
 });
+Route::group([
+    'prefix' => 'home',
+        ],
+        function() {
+    Route::get('/', 'HomeController@index');
+    Route::get('/post/{slug}', 'HomeController@show')->name('post.show');
+    Route::get('/tag/{slug}', 'HomeController@tag')->name('tag.show');
+    Route::get('/category/{slug}', 'HomeController@category')->name('category.show');
+    Route::post('/subscribe', 'SubsController@subscribe');
+    Route::get('/verify/{token}', 'SubsController@verify');
+});
 
-Route::get('/home', 'HomeController@index');
-Route::get('/post/{slug}', 'HomeController@show')->name('post.show');
-Route::get('/tag/{slug}', 'HomeController@tag')->name('tag.show');
-Route::get('/category/{slug}', 'HomeController@category')->name('category.show');
-Route::post('/subscribe', 'SubsController@subscribe');
-Route::get('/verify/{token}', 'SubsController@verify');
 
-Route::group(['middleware' => 'auth'], function() {
+Route::group([
+    'prefix' => 'home',
+    'middleware' => 'auth'
+    ], function() {
     Route::get('/logout', 'AuthController@logout');
     Route::get('/profile', 'ProfileController@index');
     Route::post('/profile', 'ProfileController@store');
     Route::post('/comment', 'CommentsController@store');
 });
 
-Route::group(['middleware' => 'guest'], function() {
+Route::group([
+    'prefix' => 'home',
+    'middleware' => 'guest'
+    ], function() {
     Route::get('/register', 'AuthController@registerForm');
     Route::post('/register', 'AuthController@register');
     Route::get('/login', 'AuthController@loginForm');
