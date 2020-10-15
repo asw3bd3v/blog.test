379fcb187d508a9742a3741644ce4e56a4c73974
asw3bd3v <asw3bd3v@gmail.com>
2020-10-15 10:42:33 +0300

    Добавлен тег meta с именем csrf-token

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="csrf-token" content="<?= csrf_token(); ?>" id="token">
   <title>Laravel React application</title>
 <!--  <link href="{{mix('react/css/app.css')}}" rel="stylesheet" type="text/css">-->
 <link href="/react/css/app.css" rel="stylesheet" type="text/css">
